8308cd739c3d2ca3001c6d5f6d00d7509b8c346b
Dmitry162 <dmtr_ann@mail.ru>
2020-05-16 19:48:08 +0300

    in main.c add parser of command line

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include "stdio.h"
 
 int main(int argc, char* argv[]) {
-    printf("Hello, world! Writing in MinGW");
+	char *sAddTitle=NULL;
+	if(argc>1)
+{
+		sAddTitle=argv[1];
+fprintf(stderr,"Hello, %s",sAddTitle);
+}
+else
+	fprintf(stderr,"Hello, %s",argv[0]);
+    //printf("Hello, world! Writing in MinGW");
+	
     return 0;
 }
